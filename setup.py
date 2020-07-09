d1278263bdb91d787637f2586d9432de0bfcdbfd
GoBig87 <nobody5390@yahoo.com>
2020-07-09 12:13:34 -0700

    Removing readme

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     install_requires = [
         'pylint',
@@ -14,10 +11,9 @@ setuptools.setup(
     author_email     = 'python@thieding.com',
     maintainer       = 'Tyler N. Thieding',
     maintainer_email = 'python@thieding.com',
-    url              = 'https://github.com/TNThieding/pylint-fail-under',
+    url              = 'https://github.com/GoBig87/pylint-fail-under',
     description      = 'Pylint wrapper that verifies code reaches a minimum quality score.',
-    long_description = long_description,
-    download_url     = 'https://github.com/TNThieding/pylint-fail-under',
+    download_url     = 'https://github.com/GoBig87/pylint-fail-under',
     classifiers      = [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
